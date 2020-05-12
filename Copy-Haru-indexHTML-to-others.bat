3f271089e0cfd64532d465367f508111ced5b00b
Eden~♡ <crash333@hotmail.de>
2020-05-12 11:31:55 +0200

    Update Copy-Haru-indexHTML-to-others.bat

--- Copy-Haru-indexHTML-to-others.bat
+++ Copy-Haru-indexHTML-to-others.bat
@@ -1,11 +1,11 @@
 @echo off
 cd /d %~dp0
 
-copy /y "docs\haru\index.html" "docs\erwin\index.html"
-copy /y "docs\haru\index.html" "docs\lily\index.html"
-copy /y "docs\haru\index.html" "docs\stella\index.html"
-copy /y "docs\haru\index.html" "docs\jin\index.html"
-copy /y "docs\haru\index.html" "docs\iris\index.html"
-copy /y "docs\haru\index.html" "docs\chii\index.html"
+copy /y "docs\Haru\index.html" "docs\Erwin\index.html"
+copy /y "docs\Haru\index.html" "docs\Lily\index.html"
+copy /y "docs\Haru\index.html" "docs\Stella\index.html"
+copy /y "docs\Haru\index.html" "docs\Jin\index.html"
+copy /y "docs\Haru\index.html" "docs\Iris\index.html"
+copy /y "docs\Haru\index.html" "docs\Chii\index.html"
 
 echo. "Finished"
