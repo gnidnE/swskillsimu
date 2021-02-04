9d13d24436fe2090d0766e68f38ac366194f8f2c
Eden~♡ <crash333@hotmail.de>
2021-02-04 04:49:18 +0100

    Update EntryPoint_SkillTree.js

--- docs/assets/EntryPoint_SkillTree.js
+++ docs/assets/EntryPoint_SkillTree.js
@@ -671,7 +671,7 @@ jQuery(document).ready(function ($) {
     var cached_querstring = new URLSearchParams(window.location.search),
         showassignment = cached_querstring.get("sa"),
         clevel = cached_querstring.get("lv") || cached_querstring.get("level") || window.appdata.maxCharacterLevel,
-        param_selectedclass = cached_querstring.get("c") || ____current_SkillCore.GetAvailableClassIndex("C");
+        param_selectedclass = cached_querstring.get("c") || ____current_SkillCore.GetAvailableClassIndex();
 
     ____current_SkillCore.ReadTree(function (data) {
             if (isNaN(clevel)) {
